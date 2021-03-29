49962c8a29430334397210db3e14ee789cf2a8bd
Myles Burt <myles_burt@rocketmail.com>
2021-03-30 00:33:41 +1030

    Just added mx-auto to cols in Navbar.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,13 +6,13 @@ function Navbar() {
     <div className="grid-rows-1">
       <div className="bg-gray-500 text-white">
         <div className="grid grid-cols-12 gap-2 h-24 flex flex-wrap content-center">
-          <div className="col-span-6 ">
+          <div className="col-span-6 mx-auto">
             <p>Search box.</p>
           </div>
-          <div className="col-span-3">
+          <div className="col-span-3 mx-auto">
             <p>Timeline bar.</p>
           </div>
-          <div className="col-span-3">
+          <div className="col-span-3 mx-auto">
             <p>Radio btns.</p>
           </div>
         </div>
