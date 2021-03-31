9570c336ebe291f8b659a8a4edbdf4d3e987c2b6
Myles Burt <myles_burt@rocketmail.com>
2021-03-31 11:46:28 +1030

    Radio btns styled, can't seem to change the colour of the btns when clicked regardless of what I try. Will come back to that later.

--- client/src/components/RadioBtn/index.js
+++ client/src/components/RadioBtn/index.js
@@ -7,10 +7,22 @@ function RadioBtns() {
       <div className="p-4">
         <div>TYPE</div>
         <div>
-          <input type="radio" value="Any" name="genre" /> Any
-          <input type="radio" value="Movie" name="genre" /> Movie
-          <input type="radio" value="Series" name="genre" /> Series
-          <input type="radio" value="Episodes" name="genre" /> Episodes
+          <label className="inline-flex items-center">
+            <input type="radio" value="Any" name="genre" />
+            <h5 className="ml-1 mr-2">Any</h5>
+          </label>
+          <div className="inline-flex items-center">
+            <input type="radio" value="Movies" name="genre" />
+            <h5 className="ml-1 mr-2">Movies</h5>
+          </div>
+          <div className="inline-flex items-center">
+            <input type="radio" value="Series" name="genre" />
+            <h5 className="ml-1 mr-2">Series</h5>
+          </div>
+          <div className="inline-flex items-center">
+            <input type="radio" value="Episodes" name="genre" />
+            <h5 className="ml-1 mr-2">Episodes</h5>
+          </div>
         </div>
       </div>
     </div>
