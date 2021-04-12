91b674ecc3f715ae0b847479e49d2eb68ca4ad50
Myles Burt <myles_burt@rocketmail.com>
2021-04-13 00:42:03 +0930

    Trying to implement UseState, a lot of tutorials are using react const components rather than functional components. Finding out how to use UseState in a functional component at the present.

--- client/src/components/MovieInfo/index.js
+++ client/src/components/MovieInfo/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles.css";
 
-function MovieInfo() {
+function MovieInfo(props) {
   return (
     <div className="container">
       <div className="grid-rows-1 h-auto">
